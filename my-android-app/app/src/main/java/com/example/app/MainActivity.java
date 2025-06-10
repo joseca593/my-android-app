2216803927b82fe08be836f93f61454ff21f433f
GOICOCHEA <JGOICOCHEA@banbif.com.pe>
2025-06-10 14:47:34 -0500

    Proyecto Android mínimo

---
+++ my-android-app/app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView textView = new TextView(this);
+        textView.setText("Hello, Android!");
+        setContentView(textView);
+    }
+}
